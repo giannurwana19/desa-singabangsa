1d6f2bdc3c513cfa5ae3ff0608461f0aaae2c4d6
giannurwana19 <giannurwana19@gmail.com>
2021-09-07 21:06:40 +0700

    require navbar dan include

--- desa.com/navbar.php
+++ desa.com/navbar.php
@@ -1,130 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SingaBanga Info</title>
-
-    <!-- Bootstrap core CSS -->
-    <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
-
-
-    <!-- ////////////////// Validate ////////////////// -->
-    <script language="javascript">
-        function isNumberKey(evt) //Number
-        {
-            var charCode = (evt.which) ? evt.which : event.keyCode;
-            console.log(charCode);
-            if (charCode > 31 && (charCode < 48 || charCode > 57))
-                return false;
-
-            return true;
-        }
-
-        function isUppercaseKey(evt) //Uppercase
-        {
-            var charCode = (evt.which) ? evt.which : event.keyCode;
-            console.log(charCode);
-            if (charCode > 31 && (charCode < 65 || charCode > 90))
-                return false;
-
-            return true;
-        }
-
-        function isLowercaseKey(evt) //Lowercase
-        {
-            var charCode = (evt.which) ? evt.which : event.keyCode;
-            console.log(charCode);
-            if (charCode > 31 && (charCode < 97 || charCode > 122))
-                return false;
-
-            return true;
-        }
-
-        function isAlphabetKey(evt) //Alphabet + spc
-        {
-            var charCode = (evt.which) ? evt.which : event.keyCode;
-            console.log(charCode);
-            if (charCode > 32 && (charCode < 97 || charCode > 122))
-                return false;
-
-            return true;
-        }
-    </script>
-
-    <!-- ////////////////// End Validate ////////////////// -->
-    <link rel="shortcut icon" href="../images/logo.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
+    <nav class="navbar navbar-expand-lg font-weight-bold navbar-dark bg-blue-900 fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img src="../images/logo.png" width="40" height="40">
-                <span class="ml-2">Desa Singabangsa</span>
-            </a>
-
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <nav class="navbar navbar-dark bg-blue-900">
+                <a class="navbar-brand" href="#">
+                    <img src="../../images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                    <span class="title-navbar"><span class="d-none d-sm-inline-block">DESA</span> SINGABANGSA</span>
+                </a>
+            </nav>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=beranda">
-                            Beranda
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Profil
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
-                            <a class="dropdown-item" href="index.php?page=profil"> - Profil Desa</a>
-                            <a class="dropdown-item" href="index.php?page=profil_pemerintah"> - Profil Pemerintahan Desa</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=agenda">
-                            Agenda
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=galeri">
-                            Galeri
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=panduan">
-                            Panduan
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=contact">
-                            Fastilitas
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=contact">
-                            Kontak
-                        </a>
-                    </li>
-                </ul>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link" href="beranda.php#beranda">Beranda</a>
+                    <a class="nav-link" href="profil.php">Profil</a>
+                    <a class="nav-link" href="agenda.php">Agenda</a>
+                    <a class="nav-link" href="galeri.php">Galeri</a>
+                    <a class="nav-link" href="panduan.php">Panduan</a>
+                    <a class="nav-link" href="kontak.php">Kontak</a>
+                </div>
             </div>
         </div>
     </nav>
-    <br><br><br>
--- desa.com/page/beranda.php
+++ desa.com/page/beranda.php
@@ -50,29 +50,7 @@
 
 <body>
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg font-weight-bold navbar-dark bg-blue-900 fixed-top">
-        <div class="container">
-            <nav class="navbar navbar-dark bg-blue-900">
-                <a class="navbar-brand" href="#">
-                    <img src="../../images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-                    <span class="title-navbar"><span class="d-none d-sm-inline-block">DESA</span> SINGABANGSA</span>
-                </a>
-            </nav>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav ml-auto">
-                    <a class="nav-link" href="beranda.php#beranda">Beranda</a>
-                    <a class="nav-link" href="profil.php">Profil</a>
-                    <a class="nav-link" href="agenda.php">Agenda</a>
-                    <a class="nav-link" href="galeri.php">Galeri</a>
-                    <a class="nav-link" href="panduan.php">Panduan</a>
-                    <a class="nav-link" href="kontak.php">Kontak</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php require '../navbar.php' ?>
 
     <!-- carousel -->
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -284,39 +262,7 @@
         </div>
     </section>
 
-    <footer id="footer" class="bg-light pt-5">
-        <div class="footer-inner">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-7 mb-3 text-justify">
-                        <div class="mb-3">
-                            <h5>Desa Singabangsa Kabupaten Bogor</h5>
-                            <p>Silahkan gunakan layanan informasi masyarakat dengan bijak.</p>
-                            <p>Website desa ini adalah salah satu bentuk pelayanan perangkat desa kepada masyarakat Desa Singabangsa maupun masyarakat di luar Desa</p>
-                        </div>
-                        <div class="mt-3">
-                            <h5>Informasi dan Pelayanan</h5>
-                            <p><strong>Kantor Kepala Desa</strong> - Singabangsa Rt. 004 Te. 002, Singabangsa, Tenjo, Bogor, Jawa Barat 16370<br></p>
-                            <p><strong>Telpon</strong> - (123) 456-7890</p>
-                            <p><strong>E-Mail</strong> - <a href="mailto:name@example.com">info@singabangsa.desa.id</a></p>
-                            <p><strong>Waktu Pelayanan</strong> - Senin - Jumat Pukul 09:00 sampai 17:00 WIB.</p>
-                            <p>Klik tombol "Pengaduan" untuk layanan pengaduan atau laporan Pemerintah Kabupaten Bogor.</p>
-                            <a href="http://pengaduan.bogorkab.go.id" target="_blank" class="btn btn-primary">Pengaduan</a>
-                        </div>
-                    </div>
-                    <div class="col-md-5">
-                        <div class="fb-frame">
-                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbogorkab%2F&amp;tabs=timeline&amp;width=340&amp;height=300&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=572660399555941" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="1" allow="encrypted-media"></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-outer bg-blue-900 text-center py-3">
-            <small class="text-blue-50">&copy;Copyright <span id="year"></span> | Desa Singabangsa</small>
-        </div>
-    </footer>
-
+    <?php require '../footer.php' ?>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
