f0bef7b73acc3e1c7f3a7f96d86a6dd1bda9c153
novaramadhona <novaramadhona2000@gmail.com>
2021-09-08 11:11:54 +0700

    require footer dan menghapus aos

--- desa.com/page/kontak.php
+++ desa.com/page/kontak.php
@@ -53,7 +53,7 @@
     <?php require '../navbar.php' ?>
  
     <section id="kontak" class="py-5">
-        <div class="mt-5 container">
+        <div class="container pt-5">
             <h4 class="pb-4 text-center">HUBUNGI KAMI</h4>
             <div class="row">
                 <div class="col-lg-7 mb-4">
@@ -114,38 +114,7 @@
         </div>
     </section>
 
-    <footer id="footer" class="bg-light pt-5 mt-4">
-        <div class="footer-inner">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-7 mb-3 text-justify">
-                        <div class="mb-3">
-                            <h5>Desa Singabangsa Kabupaten Bogor</h5>
-                            <p>Silahkan gunakan layanan informasi masyarakat dengan bijak.</p>
-                            <p>Website desa ini adalah salah satu bentuk pelayanan perangkat desa kepada masyarakat Desa Singabangsa maupun masyarakat di luar Desa</p>
-                        </div>
-                        <div class="mt-3">
-                            <h5>Informasi dan Pelayanan</h5>
-                            <p><strong>Kantor Kepala Desa</strong> - Singabangsa Rt. 004 Te. 002, Singabangsa, Tenjo, Bogor, Jawa Barat 16370<br></p>
-                            <p><strong>Telpon</strong> - (123) 456-7890</p>
-                            <p><strong>E-Mail</strong> - <a href="mailto:name@example.com">info@singabangsa.desa.id</a></p>
-                            <p><strong>Waktu Pelayanan</strong> - Senin - Jumat Pukul 09:00 sampai 17:00 WIB.</p>
-                            <p>Klik tombol "Pengaduan" untuk layanan pengaduan atau laporan Pemerintah Kabupaten Bogor.</p>
-                            <a href="http://pengaduan.bogorkab.go.id" target="_blank" class="btn btn-primary">Pengaduan</a>
-                        </div>
-                    </div>
-                    <div class="col-md-5">
-                        <div class="fb-frame">
-                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbogorkab%2F&amp;tabs=timeline&amp;width=340&amp;height=300&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=572660399555941" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="1" allow="encrypted-media"></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-outer bg-blue-900 text-center py-3">
-            <small class="text-blue-50">&copy;Copyright <span id="year"></span> | Desa Singabangsa</small>
-        </div>
-    </footer>
+    <?php require '../footer.php' ?>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
